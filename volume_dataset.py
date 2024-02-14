c0cae317a91ccf6db6d6d18bb02386fc571ca684
mahshid <mahshid.shiri1998@gmail.com>
2024-02-14 19:21:02 +0100

    minor change

--- volume_dataset.py
+++ volume_dataset.py
@@ -40,6 +40,7 @@ class Volume_Dataset(Dataset):
 
     def __getitem__(self, idx):
         img = np.load(self.data_dir+self.sid_list[idx])
-        print(self.sid_list[idx])
         class_label = self.class_label_dict.get(self.sid_list[idx], -1) # -1 if no class label
+        if class_label == -1:
+            print(self.sid_list[idx])
         return img[None,:,:,:], class_label
